1f3391b6eaeb3b97afa654134f475661a6c42d3c
steve passarelli <passarellism@gmail.com>
2023-01-13 16:56:21 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+function isWin(record) {
+    if (record.result === "W") {
+        return true
+    }else if (record.result != "W"){
+    }
+}
+
+function superbowlWin(record) {
+    const bowl = record.find(isWin)
+    if (typeof bowl === 'object') {
+        return bowl.year
+    } else if (typeof bowl != 'object') {
+        return
+    }
+}
